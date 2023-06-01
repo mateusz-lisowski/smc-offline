1936c85bd1033ea0d42414f324294e0878922439
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-06-02 00:59:31 +0100

    Added electronics dashboard

--- resources/app/main.py
+++ resources/app/main.py
@@ -16,3 +16,11 @@ def show_ship():
     path = '/code/app/res/ship.jpg'
 
     return FileResponse(path)
+
+
+@app.get("/rocket_schema")
+def show_rocket_schema():
+
+    path = '/code/app/res/rocket_schema.png'
+
+    return FileResponse(path)
